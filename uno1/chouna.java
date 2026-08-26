aba84d1f7ffee7da0496a1f9e3f17ae78bd707ce
Heriberto Mireles Mosqueda <herymimo11@gmail.com>
2026-08-26 17:55:04 +0000

    unoo

---
+++ uno1/chouna.java
@@ -0,0 +1,17 @@
+public class chouna {
+int [] chouna = new int[10];
+        int choun[]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        public void asicnar(){
+        for(int i=0; i<=9; i++){
+            chouna[ i ] = i+3;
+
+        }}
+**metodo print
+
+    public static void main(String[] args) {
+        
+
+        
+    }
+}
