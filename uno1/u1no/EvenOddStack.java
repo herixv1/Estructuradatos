f116f87100b1911db1306df6f959b2aaddd7dd30
Herixv1 <herymimo11@gmail.com>
2026-08-31 10:26:01 -0700

    uno.4

---
+++ uno1/u1no/EvenOddStack.java
@@ -0,0 +1,19 @@
+package u1no;
+
+public class EvenOddStack {
+
+    public void asicnar() {
+
+    }
+
+    public void show() {
+
+    }
+
+    public static void main(String[] args) {
+        EvenOddStack s = new EvenOddStack();
+        s.asicnar();
+        s.show();
+    }
+
+}
