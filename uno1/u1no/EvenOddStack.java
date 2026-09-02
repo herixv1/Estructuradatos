1cf9228ec744d20e094d20ede95e57c3c1eb7a25
Herixv1 <herymimo11@gmail.com>
2026-09-02 15:30:40 -0700

    uno.5-1

--- uno1/u1no/EvenOddStack.java
+++ uno1/u1no/EvenOddStack.java
@@ -1,13 +1,25 @@
 package u1no;
 
+import java.util.Stack;
+
 public class EvenOddStack {
 
-    public void asicnar() {
+    Stack<Integer> even = new Stack();
+    Stack<Integer> odd = new Stack();
 
+    public void asicnar() {
+        for (int i = 0; i < 50; i++) {
+            if (i % 2 == 0) {
+                even.push(i);
+            } else {
+                odd.push(i);
+            }
+        }
     }
 
     public void show() {
-
+        System.out.println("Pila de Pares: " + even);
+        System.out.println("Pila de Impares: " + odd);
     }
 
     public static void main(String[] args) {
--- uno1/u1no/Pilas.java
+++ uno1/u1no/Pilas.java
@@ -1,6 +1,7 @@
-package unidad1;
+package u1no;
 
 import java.util.Stack;
+
 public class Pilas {
 
     public static void main(String[] args) {
@@ -8,15 +9,13 @@ public class Pilas {
         System.out.println("creando pila perrona....");
 
         Stack<Integer> pila = new Stack<Integer>();
-        
+
         System.out.println("Tamaño de la pila: " + pila.size());
 
         System.out.println("Agregando elementos a la pila...");
 
-        for(int i=0; i<10; i++)
-            pila.push(i+1);
-        
-        
+        for (int i = 0; i < 10; i++)
+            pila.push(i + 1);
 
         System.out.println("Tamaño de la pila: " + pila.size());
         System.out.println("valores de la pila: " + pila);
